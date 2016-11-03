9c0fc423f9afd97d9d4ee699071cf7a4cf9c8886
hcarlson1 <hcarlson1@css.edu>
2016-11-03 11:40:57 -0500

    Request 407 for url capabilities.

--- SingleResponsibilityPrinciple/Program.cs
+++ SingleResponsibilityPrinciple/Program.cs
@@ -13,8 +13,10 @@ namespace SingleResponsibilityPrinciple
         {
             var tradeStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("SingleResponsibilityPrinciple.trades4.txt");
 
+            String url = "http://faculty.css.edu/tgibbons/trades4.txt";
             var tradeProcessor = new TradeProcessor();
-            tradeProcessor.ProcessTrades(tradeStream);
+            //tradeProcessor.ProcessTrades(tradeStream);
+            tradeProcessor.ProcessTrades(url);
 
             Console.ReadKey();
         }
